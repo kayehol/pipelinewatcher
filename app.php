99dbfac908e6f53eb46ef2b9fa9148030fcae0da
kayehol <kaye.holanda@gmail.com>
2022-08-24 10:11:32 -0300

    Bugs bugs bugs

--- app.php
+++ app.php
@@ -5,8 +5,6 @@ include __DIR__.'/vendor/autoload.php';
 use Discord\Builders\MessageBuilder;
 use Discord\Discord;
 use Discord\Parts\Channel\Message;
-use Discord\Parts\Channel\Channel;
-use Discord\Parts\Guild\Guild;
 use Discord\WebSockets\Event;
 
 $env = parse_ini_file('.env');
@@ -18,9 +16,10 @@ $discord = new Discord([
 
 $discord->on('ready', function(Discord $discord){
     echo "Watching...\n";
-    $successGifs = ['success1', 'success2', 'success3'];
+    $successGifs = array_diff(scandir('./gifs/success'), array('.','..'));
+    $failGifs = array_diff(scandir('./gifs/fail'), array('.','..'));
+    
     $successMsgs = ['Aí sim', 'Boa', 'Show'];
-    $failGifs = ['fail1','fail2','fail3','fail4','fail5'];
     $failMsgs = ['Vish...','Xiii', 'Ihh...','Lascou'];
 
     $discord->on(Event::MESSAGE_CREATE, function(Message $message, Discord $discord) use ($successGifs, $successMsgs, $failGifs, $failMsgs){
@@ -32,12 +31,12 @@ $discord->on('ready', function(Discord $discord){
         if (str_contains($content, 'has failed')) {
             $message->channel->sendMessage(MessageBuilder::new()
                 ->setContent($failMsgs[array_rand($failMsgs)])
-                ->addFile('./'.$failGifs[array_rand($failGifs)].'.gif')
+                ->addFile('./gifs/fail/'.$failGifs[array_rand($failGifs)])
             );
         } elseif (str_contains($content, 'has passed')) {
             $message->channel->sendMessage(MessageBuilder::new()
                 ->setContent($successMsgs[array_rand($successMsgs)])
-                ->addFile('./'.$successGifs[array_rand($successGifs)].'.gif')
+                ->addFile('./gifs/success/'.$successGifs[array_rand($successGifs)])
             );
         }
     });
